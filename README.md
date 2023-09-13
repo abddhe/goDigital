38ce13996af91a5395ca4ff7a5d410280f756082
Abdullah Dheir <tareqm577@gmail.com>
2023-09-13 11:51:47 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hi, You will find a goDigital assignments (Flutter)
+
+## Screenshots
+
+- Assignment #1
+![Abdullah Ashraf Dheir](/screenshots/assignment 1/1.png "Assignment #1 - 1")
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:first_project/screens/first_assignment.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'First Project',
+      home: FirstAssignment(),
+    );
+  }
+}
